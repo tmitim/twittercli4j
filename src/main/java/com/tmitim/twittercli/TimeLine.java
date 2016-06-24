e527ef534b9fb7f3e08f6928bf9d523b2436546e
tmitim <igetemail@gmail.com>
2016-06-23 19:17:34 -0700

    Add flag to get specific user's timeline

--- src/main/java/com/tmitim/twittercli/TimeLine.java
+++ src/main/java/com/tmitim/twittercli/TimeLine.java
@@ -1,9 +1,11 @@
 package com.tmitim.twittercli;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.github.rvesse.airline.annotations.Command;
+import com.github.rvesse.airline.annotations.Option;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -16,17 +18,27 @@ import twitter4j.TwitterFactory;
 @Command(name = "timeline", description = "Get your timeline")
 public class TimeLine implements Runnable {
 
-	public void run() {
+	@Option(name = { "-u", "--user" }, description = "get the timeline for a specific username")
+	private String username;
 
-		System.out.println("Pulling your timeline.");
-		Twitter twitter = TwitterFactory.getSingleton();
+	@Override
+	public void run() {
 
 		List<Status> statuses = new ArrayList<>();
 		try {
-			statuses = twitter.getHomeTimeline();
+			Twitter twitter = TwitterFactory.getSingleton();
+			if ( username != null && !username.isEmpty() ) {
+				System.out.println("Pulling @" + username + "'s timeline...");
+				statuses = twitter.getUserTimeline(username);
+			} else {
+				System.out.println("Pulling your home timeline...");
+				statuses = twitter.getHomeTimeline();
+			}
 		} catch (TwitterException e) {
-			e.printStackTrace();
+			System.out.println(e.getErrorMessage());
 		}
+
+		Collections.reverse(statuses);
 		for (Status status : statuses) {
 			System.out.println(status.getUser().getName() + " (@" + status.getUser().getScreenName() + "): ");
 			System.out.println(status.getText());
