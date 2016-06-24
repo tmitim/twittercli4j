f81187b8d1e2bd4efcf1a451756af9c556b9a270
tmitim <igetemail@gmail.com>
2016-06-23 17:41:32 -0700

    Add id, favorite + rt count to timeline

--- src/main/java/com/tmitim/twittercli/TimeLine.java
+++ src/main/java/com/tmitim/twittercli/TimeLine.java
@@ -31,6 +31,14 @@ public class TimeLine implements Runnable {
 			System.out.println(status.getUser().getName() + " (@" + status.getUser().getScreenName() + "): ");
 			System.out.println(status.getText());
 			System.out.println(status.getCreatedAt());
+			System.out.println(
+				String.format(
+					"ID: %d, RT: %d, <3: %d",
+					status.getId(),
+					status.getRetweetCount(),
+					status.getFavoriteCount()
+				)
+			);
 			System.out.println("");
 		}
 	}
