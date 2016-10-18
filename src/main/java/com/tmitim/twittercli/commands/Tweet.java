9f5c0dd869cd2f9e9163831924d436d8102fee81
tmitim <igetemail@gmail.com>
2016-10-18 00:24:26 -0700

    Remove unused argument/flag when tweeting

--- src/main/java/com/tmitim/twittercli/commands/Tweet.java
+++ src/main/java/com/tmitim/twittercli/commands/Tweet.java
@@ -7,8 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import com.github.rvesse.airline.SingleCommand;
 import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Command;
-import com.github.rvesse.airline.annotations.Option;
-
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -21,9 +19,6 @@ import twitter4j.TwitterFactory;
 @Command(name = "tweet", description = "Tweet your thoughts")
 public class Tweet implements Runnable {
 
-	@Option(name = { "-f", "--flag" }, description = "An option that requires no values")
-	private boolean flag = false;
-
 	@Arguments(description = "Additional arguments")
 	private List<String> args;
 
@@ -33,6 +28,7 @@ public class Tweet implements Runnable {
 		cmd.run();
 	}
 
+	@Override
 	public void run() {
 		Twitter twitter = TwitterFactory.getSingleton();
 
@@ -41,12 +37,7 @@ public class Tweet implements Runnable {
 			System.out.println("Successfully updated the status to [" + status.getText() + "].");
 
 		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		System.out.println("Flag was " + (this.flag ? "set" : "not set"));
-		if (args != null)
-			System.out.println("Arguments were " + StringUtils.join(args, " "));
 	}
 }
