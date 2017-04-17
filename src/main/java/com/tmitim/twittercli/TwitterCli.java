5480300970f1f329ad0b463dba9e1b5cece135a1
tmitim <igetemail@gmail.com>
2017-04-16 19:39:25 -0700

    Add version flag

--- src/main/java/com/tmitim/twittercli/TwitterCli.java
+++ src/main/java/com/tmitim/twittercli/TwitterCli.java
@@ -1,5 +1,8 @@
 package com.tmitim.twittercli;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import com.github.rvesse.airline.annotations.Cli;
 import com.tmitim.twittercli.commands.DirectMessage;
 import com.tmitim.twittercli.commands.Favorite;
@@ -8,6 +11,7 @@ import com.tmitim.twittercli.commands.Location;
 import com.tmitim.twittercli.commands.TimeLineCommand;
 import com.tmitim.twittercli.commands.Trend;
 import com.tmitim.twittercli.commands.Tweet;
+
 import com.tmitim.twittercli.commands.Search;
 
 @Cli(
@@ -20,6 +24,13 @@ public class TwitterCli {
 	public static void main(String[] args) {
 
 		com.github.rvesse.airline.Cli<Runnable> cli = new com.github.rvesse.airline.Cli<>(TwitterCli.class);
+		
+		if (Stream.of(args).anyMatch(x -> x.equals("--version") || x.equals(("-v")))) {
+			String version = TwitterCli.class.getPackage().getImplementationVersion();
+			System.out.println(String.format("TwitterCLI4j: %s", version));
+			return;
+		}
+		
 		try {
 			Runnable cmd = cli.parse(args);
 			cmd.run();
