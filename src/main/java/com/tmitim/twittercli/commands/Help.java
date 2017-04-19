aa04b88090397b7c19e89916bc17ad4510131114
tmitim <igetemail@gmail.com>
2017-04-18 23:19:25 -0700

    Remove redundant version flag in help

--- src/main/java/com/tmitim/twittercli/commands/Help.java
+++ src/main/java/com/tmitim/twittercli/commands/Help.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 
 import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Command;
-import com.github.rvesse.airline.annotations.Option;
 import com.github.rvesse.airline.annotations.help.BashCompletion;
 import com.github.rvesse.airline.help.cli.bash.CompletionBehaviour;
 import com.github.rvesse.airline.model.GlobalMetadata;
@@ -23,19 +22,10 @@ public class Help implements Runnable {
 	@BashCompletion(behaviour = CompletionBehaviour.CLI_COMMANDS)
 	private List<String> commandNames = new ArrayList<String>();
 
-	@Option(name = { "-v", "--version" }, description = "twittercli version")
-	private boolean getVersion;
-
 	@Override
 	public void run() {
 		try {
-			if (getVersion) {
-				String version = this.getClass().getPackage().getImplementationVersion();
-				System.out.println(String.format("TwitterCLI4j: %s", version));
-			} else {
-				com.github.rvesse.airline.help.Help.help(global, commandNames);
-			}
-
+			com.github.rvesse.airline.help.Help.help(global, commandNames);
 		} catch (IOException e) {
 			System.err.println("Failed to output help: " + e.getMessage());
 			e.printStackTrace(System.err);
